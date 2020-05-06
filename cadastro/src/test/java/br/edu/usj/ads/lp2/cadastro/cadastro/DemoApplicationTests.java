b110bbc83940acea7969977c99333b78d14bbd4a
vinicius-SM <29viniciussm@gmail.com>
2020-05-05 21:51:05 -0300

    add historico

---
+++ cadastro/src/test/java/br/edu/usj/ads/lp2/cadastro/cadastro/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.usj.ads.lp2.cadastro.cadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- calculadora/src/main/java/br/edu/usj/ads/lp2/calculadora/Calculadora.java
+++ calculadora/src/main/java/br/edu/usj/ads/lp2/calculadora/Calculadora.java
@@ -1,5 +1,7 @@
 package br.edu.usj.ads.lp2.calculadora;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,27 +10,37 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class Calculadora {
 
+    List<String> listaOp = new ArrayList<>();
+
     @PostMapping(value = "/calcula")
     public ModelAndView postCalcula(@RequestParam String N1, @RequestParam String symbol, @RequestParam String N2) {
 
         ModelAndView modelAndView = new ModelAndView("index");
-        System.out.println("passei por aqui");
-        System.out.println(N1);
-        System.out.println(symbol);
-        System.out.println(N2);
         Double resultado = 0.0;
+        String operacao;
         if (symbol.equals("+")) {
             resultado = Double.parseDouble(N1) + Double.parseDouble(N2);
+            operacao = N1 + "+" + N2 + "=" + resultado;
+            listaOp.add(operacao);
         } else if (symbol.equals("-")) {
             resultado = Double.parseDouble(N1) - Double.parseDouble(N2);
+             operacao = N1 + "-" + N2 + "=" + resultado;
+             listaOp.add(operacao);
         } else if (symbol.equals("/")) {
             resultado = Double.parseDouble(N1) / Double.parseDouble(N2);
+             operacao = N1 + "/" + N2 + "=" + resultado;
+             listaOp.add(operacao);
         } else if (symbol.equals("*")) {
             resultado = Double.parseDouble(N1) * Double.parseDouble(N2);
+             operacao = N1 + "*" + N2 + "=" + resultado;
+             listaOp.add(operacao);
         }
+
+        
+        
         String texto = "o resultado é:" + resultado;
         modelAndView.addObject("mensagem", texto);
-
+        modelAndView.addObject("historico", listaOp);
         return modelAndView;
     }
 
